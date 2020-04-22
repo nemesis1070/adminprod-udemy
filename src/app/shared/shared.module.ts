b96d6ac5a5a1927b917870701ffd5190c85f33e6
scastro <scastro@saitec.co>
2020-04-22 11:00:31 -0500

    Subir Cambios

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { NopagefoundComponent } from '../pages/dashboard/nopagefound/nopagefound.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    NopagefoundComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumsComponent,
+    NopagefoundComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
